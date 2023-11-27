6019d68f8fdeb4cb0170f81ad7c22212f3b5d2b9
Bhuvaneshwari M <132464450+Bhuvaneshwarii-M@users.noreply.github.com>
2023-11-27 20:34:06 +0530

    Add files via upload

---
+++ Tech/TechMedZ/public/js/index.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+  
+function changeBackground1() {
+  document.body.style.background = 'url(meme.jpg) no-repeat';
+}
+
+function changeBackground2() {
+  document.body.style.background = 'url(meme.jpg) no-repeat';
+}
+function changeBackground3() {
+  document.body.style.background = 'url(meme.jpg) no-repeat';
+}
+function changeBackground4() {
+  document.body.style.background = 'url(meme.jpg) no-repeat';
+}
+
